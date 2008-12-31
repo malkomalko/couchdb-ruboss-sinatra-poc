609477b6b33817b0e4bd9faca29d633ef244a879
Robert Malko <robmalko@gmail.com>
2008-12-31 17:00:58 -0500

    this is frustrating

--- couchdb-ruboss.rb
+++ couchdb-ruboss.rb
@@ -33,14 +33,14 @@ get '/' do
   erb :index
 end
 
-get '/couchdb/:model.json' do
+get '/:model.json' do
   string = Inflector.camelize("#{params[:model]}")
   model  = Inflector.constantize(string)
   
   model.all.to_json
 end
 
-post '/couchdb/:model.json' do
+post '/:model.json' do
   string = Inflector.camelize("#{params[:model]}")
   model  = Inflector.constantize(string)
 
